9ec0943504db307142113ae8d263fc65173e1c3f
nick8604 <97799306+nick8604@users.noreply.github.com>
2022-04-01 09:44:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import {Routes, Route} from 'react-router-dom'
+import Technology from './Technology';
+import Sports from './Sports';
+import Business from './Business';
+import Entertainment from './Entertainment';
+import Health from './Health';
+import Mynews from './Mynews';
+
+export class App extends Component {
+  render() {
+    return (
+     <>
+       <Navbar/>
+       <Routes>
+         <Route exact path='/' element={<Mynews/>} />
+         <Route exact path='/technology' element={<Technology/>} />
+         <Route exact path='/sports' element={<Sports/>} />
+         <Route exact path='/business' element={<Business/>} />
+         <Route exact path='/entertainment' element={<Entertainment/>} />
+         <Route exact path='/health' element={<Health/>} />
+       </Routes>
+       
+     </>
+    )
+  }
+}
+
+export default App
